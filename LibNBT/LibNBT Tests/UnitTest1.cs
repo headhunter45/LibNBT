24ac922d8cf83ff24cbad9c93dd1af90c0504a4c
headhunter45_cp <SND\headhunter45_cp>
2011-08-14 11:12:22 +0000

    Initial import Everything seems to work and I even did some automated tests.

---
+++ LibNBT/LibNBT Tests/UnitTest1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibNBT;
+using System.IO;
+using System.IO.Compression;
+
+namespace LibNBT_Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        //Sanity test it always passes
+        [TestMethod]
+        public void TestPass()
+        {
+        }
+
+        [TestMethod]
+        public void TestLoadSimpleNbt()
+        {
+            String filename = @"..\..\..\LibNBT Tests\test.nbt";
+            AbstractTag tag = TagCompound.ReadFromFile(filename);
+
+            Assert.IsNotNull(tag);
+            Assert.IsInstanceOfType(tag, typeof(TagCompound));
+            TagCompound root = tag as TagCompound;
+            Assert.AreEqual<String>("hello world", root.Name);
+
+            TagString tagStr = root.GetString("name");
+            Assert.AreEqual<String>("name", tagStr.Name);
+
+            Assert.AreEqual<String>("Bananrama", tagStr.Value);
+        }
+
+        [TestMethod]
+        public void TestLoadComplexNbt()
+        {
+            String filename = @"..\..\..\LibNBT Tests\bigtest.nbt";
+            AbstractTag tag = TagCompound.ReadFromFile(filename);
+
+            Assert.IsNotNull(tag);
+            Assert.IsInstanceOfType(tag, typeof(TagCompound));
+            TagCompound level = tag as TagCompound;
+            Assert.AreEqual<String>("Level", level.Name);
+
+            TagShort shortTest = level.GetShort("shortTest");
+            Assert.IsNotNull(shortTest);
+            Assert.AreEqual<String>("shortTest", shortTest.Name);
+            Assert.AreEqual<short>(32767, shortTest.Value);
+
+            TagLong longTest = level.GetLong("longTest");
+            Assert.IsNotNull(longTest);
+            Assert.AreEqual<String>("longTest", longTest.Name);
+            Assert.AreEqual<long>(9223372036854775807, longTest.Value);
+
+            TagFloat floatTest = level.GetFloat("floatTest");
+            Assert.IsNotNull(floatTest);
+            Assert.AreEqual<String>("floatTest", floatTest.Name);
+            Assert.AreEqual<float>(0.49823147f, floatTest.Value);
+
+            TagString stringTest = level.GetString("stringTest");
+            Assert.IsNotNull(stringTest);
+            Assert.AreEqual<String>("stringTest", stringTest.Name);
+            Assert.AreEqual<String>("HELLO WORLD THIS IS A TEST STRING ÅÄÖ!", stringTest.Value);
+
+            TagInt intTest = level.GetInt("intTest");
+            Assert.IsNotNull(intTest);
+            Assert.AreEqual<String>("intTest", intTest.Name);
+            Assert.AreEqual<int>(2147483647, intTest.Value);
+
+            TagCompound nestedCompoundTest = level.GetCompound("nested compound test");
+            Assert.IsNotNull(nestedCompoundTest);
+            Assert.AreEqual<String>("nested compound test", nestedCompoundTest.Name);
+
+            TagCompound ham = nestedCompoundTest.GetCompound("ham");
+            Assert.IsNotNull(ham);
+            Assert.AreEqual<String>("ham", ham.Name);
+
+            TagString ham_name = ham.GetString("name");
+            Assert.IsNotNull(ham_name);
+            Assert.AreEqual<String>("name", ham_name.Name);
+            Assert.AreEqual<String>("Hampus", ham_name.Value);
+            
+            TagFloat ham_value = ham.GetFloat("value");
+            Assert.IsNotNull(ham_value);
+            Assert.AreEqual<String>("value", ham_value.Name);
+            Assert.AreEqual<float>(0.75f, ham_value.Value);
+
+            TagCompound egg = nestedCompoundTest.GetCompound("egg");
+            Assert.IsNotNull(egg);
+            Assert.AreEqual<String>("egg", egg.Name);
+
+            TagString egg_name = egg.GetString("name");
+            Assert.IsNotNull(egg_name);
+            Assert.AreEqual<String>("name", egg_name.Name);
+            Assert.AreEqual<String>("Eggbert", egg_name.Value);
+            
+            TagFloat egg_value = egg.GetFloat("value");
+            Assert.IsNotNull(egg_value);
+            Assert.AreEqual<String>("value", egg_value.Name);
+            Assert.AreEqual<float>(0.5f, egg_value.Value);
+
+            TagByte byteTest = level.GetByte("byteTest");
+            Assert.IsNotNull(byteTest);
+            Assert.AreEqual<String>("byteTest", byteTest.Name);
+            Assert.AreEqual<byte>(0x7f, byteTest.Value);
+
+            TagDouble doubleTest = level.GetDouble("doubleTest");
+            Assert.IsNotNull(doubleTest);
+            Assert.AreEqual<String>("doubleTest", doubleTest.Name);
+            Assert.AreEqual<double>(0.4931287132182315, doubleTest.Value);
+
+            TagList listTest_long = level.GetList("listTest (long)");
+            Assert.IsNotNull(listTest_long);
+            Assert.AreEqual<String>("listTest (long)", listTest_long.Name);
+            Assert.IsNotNull(listTest_long.Value);
+            Assert.AreEqual<int>(5, listTest_long.Value.Count);
+            Assert.AreEqual<long>(11, (listTest_long.Value[0] as TagLong).Value);
+            Assert.AreEqual<long>(12, (listTest_long.Value[1] as TagLong).Value);
+            Assert.AreEqual<long>(13, (listTest_long.Value[2] as TagLong).Value);
+            Assert.AreEqual<long>(14, (listTest_long.Value[3] as TagLong).Value);
+            Assert.AreEqual<long>(15, (listTest_long.Value[4] as TagLong).Value);
+
+            TagList listTest_compound = level.GetList("listTest (compound)");
+            Assert.IsNotNull(listTest_compound);
+            Assert.AreEqual<String>("listTest (compound)", listTest_compound.Name); 
+            Assert.IsNotNull(listTest_compound.Value);
+            Assert.AreEqual<int>(2, listTest_compound.Value.Count);
+            TagCompound listTest_compound_tag0 = listTest_compound.Value[0] as TagCompound;
+            Assert.IsNotNull(listTest_compound_tag0);
+            TagString listTest_compound_tag0_name = listTest_compound_tag0.GetString("name");
+            Assert.IsNotNull(listTest_compound_tag0_name);
+            Assert.AreEqual<String>("name", listTest_compound_tag0_name.Name);
+            Assert.AreEqual<String>("Compound tag #0", listTest_compound_tag0_name.Value);
+            TagLong listTest_compound_tag0_createdOn = listTest_compound_tag0.GetLong("created-on");
+            Assert.IsNotNull(listTest_compound_tag0_createdOn);
+            Assert.AreEqual<String>("created-on", listTest_compound_tag0_createdOn.Name);
+            Assert.AreEqual<long>(1264099775885, listTest_compound_tag0_createdOn.Value);
+
+            TagCompound listTest_compound_tag1 = listTest_compound.Value[1] as TagCompound;
+            Assert.IsNotNull(listTest_compound_tag1);
+            TagString listTest_compound_tag1_name = listTest_compound_tag1.GetString("name");
+            Assert.IsNotNull(listTest_compound_tag1_name);
+            Assert.AreEqual<String>("name", listTest_compound_tag1_name.Name);
+            Assert.AreEqual<String>("Compound tag #1", listTest_compound_tag1_name.Value);
+            TagLong listTest_compound_tag1_createdOn = listTest_compound_tag1.GetLong("created-on");
+            Assert.IsNotNull(listTest_compound_tag1_createdOn);
+            Assert.AreEqual<String>("created-on", listTest_compound_tag1_createdOn.Name);
+            Assert.AreEqual<long>(1264099775885, listTest_compound_tag1_createdOn.Value);
+
+            TagByteArray byteArrayTest = level.GetByteArray("byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))");
+            Assert.IsNotNull(byteArrayTest);
+            Assert.AreEqual<String>("byteArrayTest (the first 1000 values of (n*n*255+n*7)%100, starting with n=0 (0, 62, 34, 16, 8, ...))", byteArrayTest.Name);
+            Assert.IsNotNull(byteArrayTest.Value);
+            Assert.AreEqual<int>(1000, byteArrayTest.Value.Length);
+        }
+
+        [TestMethod]
+        public void TestSave()
+        {
+            String filename = @"..\..\..\LibNBT Tests\bigtest.nbt";
+            TagCompound tag = TagCompound.ReadFromFile(filename) as TagCompound;
+
+            MemoryStream ms = new MemoryStream();
+            MemoryStream ms2;
+            FileStream fs = File.OpenRead(filename);
+            GZipStream gzStream = new GZipStream(fs, CompressionMode.Decompress);
+            
+            tag.Write(ms);
+
+            ms2 = new MemoryStream((int)ms.Length);
+            byte[] buffer = new byte[ms.Length];
+            Assert.AreEqual<long>(ms.Length, gzStream.Read(buffer, 0, (int)ms.Length));
+
+            Assert.AreEqual<int>(-1, gzStream.ReadByte());
+            byte[] buffer2 = ms.GetBuffer();
+
+            FileStream fs2 = File.OpenWrite(@"..\..\..\LibNBT Tests\savetest.raw");
+            fs2.Write(buffer2, 0, (int)ms.Length);
+            for (long i = 0; i < ms.Length; i++)
+            {
+                Assert.AreEqual<byte>(buffer[i], buffer2[i]);
+            }
+        }
+    }
+}
---
+++ LibNBT/LibNBT/AbstractTag.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Compression;
+
+namespace LibNBT
+{
+    public enum TagType{End, Byte, Short, Int, Long, Float, Double, ByteArray, String, List, Compound};
+
+    public abstract class AbstractTag
+    {
+        public abstract TagType Type { get; }
+        public virtual String Name { get; set; }
+
+        public abstract void Write(Stream output);
+        public abstract void WriteUnnamed(Stream output);
+        
+        public static AbstractTag Read(Stream input)
+        {
+            int temp = input.ReadByte();
+            if(temp != (temp & 0xFF)){
+                throw new Exception();
+            }
+
+            switch ((TagType)temp)
+            {
+                case TagType.End:
+                    return new TagEnd();
+                case TagType.Byte:
+                    return new TagByte(input);
+                case TagType.Short:
+                    return new TagShort(input);
+                case TagType.Int:
+                    return new TagInt(input);
+                case TagType.Long:
+                    return new TagLong(input);
+                case TagType.Float:
+                    return new TagFloat(input);
+                case TagType.Double:
+                    return new TagDouble(input);
+                case TagType.ByteArray:
+                    return new TagByteArray(input);
+                case TagType.String:
+                    return new TagString(input);
+                case TagType.List:
+                    return new TagList(input);
+                case TagType.Compound:
+                    return new TagCompound(input);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
